3385baa11262a83c52c2fcb4986d72d4097ca6bd
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-13 13:33:17 +0900

    Add onPlay and onStop function

--- app/src/main/java/com/kbokka/android/servicesample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/servicesample/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.kbokka.android.servicesample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  fun onPlayButtonClick(view: View) {
+    val intent = Intent(applicationContext, SoundManageService::class.java)
+    startService(intent)
+
+    findViewById<Button>(R.id.btPlay).isEnabled = false
+    findViewById<Button>(R.id.btStop).isEnabled = true
+  }
+
+  fun onStopButtonClick(view: View) {
+    val intent = Intent(applicationContext, SoundManageService::class.java)
+    stopService(intent)
+
+    findViewById<Button>(R.id.btPlay).isEnabled = true
+    findViewById<Button>(R.id.btStop).isEnabled = false
   }
 }
